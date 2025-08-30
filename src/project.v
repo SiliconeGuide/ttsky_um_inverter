9782929aa20b758ff0a53f00785d2012b805c76e
SiliconeGuide <189062873+SiliconeGuide@users.noreply.github.com>
2025-08-30 21:45:30 +0200

    editting names again

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_inverter (
+module ttsky_um_inverter (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
     input  wire       VAPWR,    // 3.3v power supply
